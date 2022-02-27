8c7a8982634f1cc46f76d4418d95a9ec199aed55
Jonathan Robinson Manchego Sosa <jonathan571999@gmail.com>
2022-02-27 10:53:46 -0500

    lista actualizada

--- src/app/components/clientes/clientes.module.ts
+++ src/app/components/clientes/clientes.module.ts
@@ -11,17 +11,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
-  declarations: [
-    CreateClientComponent,
-    ListClientComponent
-  ],
+  declarations: [CreateClientComponent, ListClientComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
+    MatExpansionModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
@@ -29,7 +28,7 @@ import {MatSelectModule} from '@angular/material/select';
     MatDatepickerModule,
     ClientesRoutingModule,
     MatSlideToggleModule,
-    MatSelectModule
-  ]
+    MatSelectModule,
+  ],
 })
-export class ClientesModule { }
+export class ClientesModule {}
--- src/app/components/clientes/list-client/list-client.component.ts
+++ src/app/components/clientes/list-client/list-client.component.ts
@@ -1,20 +1,24 @@
 import { ClientstStateService } from './../../../services/clients-state.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Client } from 'src/app/commons/models/client';
 
 @Component({
   selector: 'app-list-client',
   templateUrl: './list-client.component.html',
-  styleUrls: ['./list-client.component.scss']
+  styleUrls: ['./list-client.component.scss'],
 })
 export class ListClientComponent implements OnInit {
   clientsObservable: Observable<Array<any>> = new Observable<Array<any>>();
   avgAges = 0;
   devStandar = 0;
-  constructor(private readonly clientsStateService: ClientstStateService) { }
+  clients: Array<Client> = [];
+  constructor(private readonly clientsStateService: ClientstStateService) {}
 
   ngOnInit(): void {
-    this.clientsObservable = this.clientsStateService.items;
+    this.clientsStateService.items.subscribe((res: Array<Client>) => {
+      this.clients = res;
+    });
     /* this.clientsStateService.observableList().subscribe(
       () => {
         this.setAvgAges();
@@ -23,26 +27,22 @@ export class ListClientComponent implements OnInit {
   }
   setAvgAges(): void {
     let avg = 0;
-    this.clientsObservable.subscribe(
-      (items) => {
-        const ages = items.map(client => +client.age)
-        avg = ages.reduce(
-          (prevClient: number, nextClient: number) =>
-          (
-            prevClient + nextClient
-          )
-          , 0);
-        this.avgAges = avg / items.length;
-        this.setDesvStandar(ages);
-      }
-    )
+    this.clientsObservable.subscribe((items) => {
+      const ages = items.map((client) => +client.age);
+      avg = ages.reduce(
+        (prevClient: number, nextClient: number) => prevClient + nextClient,
+        0
+      );
+      this.avgAges = avg / items.length;
+      this.setDesvStandar(ages);
+    });
   }
   setDesvStandar(ages: Array<number>): void {
     ages = ages.map((age) => {
-      return (age - this.avgAges) ** 2
-    })
+      return (age - this.avgAges) ** 2;
+    });
     let sum = ages.reduce((acc, curr) => acc + curr, 0);
-    let variance = sum / ages.length
-    this.devStandar = Math.sqrt(sum / ages.length)
+    let variance = sum / ages.length;
+    this.devStandar = Math.sqrt(sum / ages.length);
   }
 }
